3f417dbb653fb57a6b8727795cc33e0c6c30d421
Nathanaël Lesdalons <nathanael.lesdalons@student.tugraz.at>
2023-02-28 15:28:23 +0100

    Some upgrade to hare level and intermediate level

--- Assets/Scripts/SwitchAnimals.cs
+++ Assets/Scripts/SwitchAnimals.cs
@@ -20,7 +20,7 @@ public class SwitchAnimals : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
 
         foreach (Transform child in transform)

32b8c6fc378054b3faed64c1f4aa04624d227fa5
Nathanaël Lesdalons <nathanael.lesdalons@student.tugraz.at>
2023-02-28 15:03:25 +0100

    mainly ok

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -7,12 +7,24 @@ public class FollowTarget : MonoBehaviour
 
     [SerializeField] private GameObject target_;
     [SerializeField] private Vector3 offset_;
-
+    [SerializeField] private float ppy_;
+    [SerializeField] private float cc_;
+    [SerializeField] private float c_;
     // Update is called once per frame
     private void Update()
     {
         // Camera follows the player with specified offset position
-        transform.position = new Vector3(target_.transform.position.x + offset_.x, transform.position.y, transform.position.z);
+        var player_pos = Camera.main.WorldToViewportPoint(target_.transform.position);
+        ppy_ = player_pos.y;
+        var y_pos = transform.position.y;
+        cc_ = (player_pos.y + 0.25f);
+        if (player_pos.y < 0.25) {
+            y_pos = Camera.main.ViewportToWorldPoint(new Vector3(0, cc_, 0)).y;
+        } else if (player_pos.y > 0.5f && target_.CompareTag("Player")) {
+            y_pos += 0.02f;
+        }
+        c_ = y_pos;
+        transform.position = new Vector3(target_.transform.position.x + offset_.x, y_pos, transform.position.z);
     }
 
     public void SetTarget(GameObject target)

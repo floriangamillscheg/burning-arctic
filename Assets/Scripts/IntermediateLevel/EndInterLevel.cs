8c680090b070b4837e51b24b21acc13e920f31d7
Nathanaël Lesdalons <nathanael.lesdalons@student.tugraz.at>
2023-03-02 20:21:28 +0100

    Link intermediate to end level

--- Assets/Scripts/IntermediateLevel/EndInterLevel.cs
+++ Assets/Scripts/IntermediateLevel/EndInterLevel.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class EndInterLevel : MonoBehaviour {
     protected void OnTriggerEnter2D(Collider2D other) {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("EndScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("EndLevel");
     }
 }
